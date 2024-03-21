9b3012b4f5dcbebf22ec8c2d53cac31f215d3b93
AbadBeniz <benizsossaabad2@gmail.com>
2024-03-20 20:43:22 -0400

    end-point Producto

---
+++ ExamenANBS/ExamenANBS/Contratos/IProveedorRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using ExamenANBS.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenANBS.Contratos
+{
+    public interface IProveedorRepositorio
+    {
+        public Task<bool> Create(Proveedor proveedor);
+        public Task<bool> Update(Proveedor proveedor);
+        public Task<bool> Delete(string partitionkey, string rowkey);
+        public Task<Proveedor> Get(string id);
+        public Task<List<Proveedor>> GetAll();
+    }
+}
